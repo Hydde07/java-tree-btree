5c9189b8812add387dc8cc6982453b66021f5fd1
Eddy Augusto Santos <54852649+Hydde07@users.noreply.github.com>
2021-12-15 00:03:44 -0300

    Add files via upload

---
+++ TrabalhoBTree/src/BTree/No.java
@@ -0,0 +1,104 @@
+package BTree;
+
+public class No {
+    private int[] info;
+    private No[] no;
+    private int TL;
+
+    public No(int n) {
+        this.info = new int[2*n+1];
+        this.no = new No[2*n+2];
+        this.TL = 0;
+    }
+
+    public No(int n, int TL) {
+        this.info = new int[2*n+1];
+        this.no = new No[2*n+2];
+        this.TL = TL;
+    }
+
+    public int[] getInfo() {
+        return info;
+    }
+
+    public int getInfo(int pos) {
+        return info[pos];
+    }
+
+    public void setInfo(int[] info) {
+        this.info = info;
+    }
+
+    public void setInfo(int info, int pos) {
+        this.info[pos] = info;
+    }
+
+    public No[] getNo() {
+        return no;
+    }
+
+    public No getNo(int pos) {
+        return no[pos];
+    }
+
+    public void setNo(No[] no) {
+        this.no = no;
+    }
+
+    public void setNo(No no, int pos) {
+        this.no[pos] = no;
+    }
+
+    public int getTL() {
+        return TL;
+    }
+
+    public void setTL(int TL) {
+        this.TL = TL;
+    }
+    
+    public void add(int n){
+        int p = getPos(n);
+        insertPosition(p);
+        info[p] = n;
+    }
+    
+    public int getPos(int n){
+        int i = 0;
+        while(i < TL && info[i] < n)
+            i++;
+        return i;
+    }
+    
+    private void insertPosition(int pos){
+        no[TL+1] = no[TL];
+        int p = TL++;
+        while(p > pos){
+            info[p] = info[p-1];
+            no[p] = no[p-1];
+            p--;
+        }
+    }
+    
+    public void deletePosition(int pos){
+        while(pos < TL-1){
+            info[pos] = info[pos+1];
+            no[pos] = no[pos+1];
+            pos++;
+        }
+        no[pos] = no[pos+1];
+        TL--;
+    }
+    
+    public boolean isFolha(){
+        return no[0] == null;
+    }
+    
+    public boolean needSplit(int n){
+        return TL > 2*n;
+    }
+    
+    public boolean insuficiente(int n){
+        return TL < n;
+    }
+}
